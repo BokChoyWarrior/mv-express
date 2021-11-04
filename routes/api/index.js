926a734af8ecb5e8ccfa5cfb7a3a01c7c27a5cef
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-11-04 22:33:34 +0000

    decrease cache memory time

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,13 +5,19 @@ const apicache = require("apicache");
 
 // We only want to cache api requests that are GET
 const onlyGetRequests = (req, res) => req.method === "GET";
-router.use(apicache.middleware("5 seconds", onlyGetRequests));
+router.use(apicache.middleware("1 seconds", onlyGetRequests));
 
 router.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS,HEAD");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,OPTIONS,HEAD"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
   next();
 });
 
